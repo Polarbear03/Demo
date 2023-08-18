46ca30539a18ae550f1bba8fe0a96b01d7d660b7
PolarBear03 <mimsic@outlook.com>
2023-08-18 13:55:05 +0800

    Initial commit

---
+++ src/A.java
@@ -0,0 +1,41 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class A {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] ars = {1,3,-1,-3,5,3,6,7};
+
+        System.out.println(solution.maxSlidingWindow(ars, 3));
+
+    }
+}
+
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) {
+            return new int[]{0};
+        }
+        Deque<Integer> deque  = new LinkedList<>();
+        int[] result = new int[nums.length - k + 1];
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+        }
+        result[0] = deque.peekFirst();
+        for (int i = k; i < nums.length; i++) {
+            if (deque.peekFirst() == nums[i - k]) {
+                deque.removeFirst();
+            }
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            result[i - k + 1] = deque.peekFirst();
+        }
+        return result;
+    }
+}
